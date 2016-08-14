d99d3872249e4bc14742715d3260c7d83d619a00
Alexandr <bumkaka@yandex.ru>
2016-08-14 22:11:13 +0300

    Initial

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+var inject          = require('gulp-inject'),
+    gulp 		    = require('gulp'),
+    browserify 	    = require('browserify'),
+    babelify 	    = require('babelify'),
+    source 		    = require('vinyl-source-stream'),
+    concat 		    = require('gulp-concat'),
+    minify          = require('gulp-minify');
+
+
+
+var jsLibs = [
+  './node_modules/jquery/dist/jquery.js',
+  './node_modules/react/dist/react-with-addons.js',
+  './node_modules/react-dom/dist/react-dom.min.js'
+];
+
+var cssLibs = [
+  './node_modules/bootstrap/dist/css/bootstrap.css'
+];
+
+
+var paths = {
+  pages: './src/pages/**/*.html'
+};
+
+gulp.task('libraries', function () {
+  gulp.src(jsLibs)
+  .pipe(concat('libs.js'))
+  .pipe(gulp.dest('./examples/js/'));
+
+  return gulp.src(cssLibs)
+  .pipe(gulp.dest('./examples/css/'));
+});
+
+
+gulp.task('build', function () {
+    gulp
+      .src('./src/index.html')
+      .pipe(gulp.dest('./examples/'));
+
+  return browserify({entries: './src/app.jsx', extensions: ['.jsx','.js'], debug: false})
+      .transform('babelify', {presets: ['es2015', 'react']})
+      .bundle()
+      .pipe(source('app.js'))
+      .pipe(gulp.dest('./examples/js/'));
+});
+
+
+
+gulp.task('release', function() {
+
+    browserify({entries: './src/react-simplex.js', extensions: ['.js'], debug: false})
+      .transform('babelify', {presets: ['es2015', 'react']})
+      .bundle()
+      .pipe(source('react-simplex.js'))
+      .pipe(gulp.dest('./dist/'))
+      
+    gulp
+      .src('./dist/react-simplex.js')
+        .pipe(minify({
+            ext:{
+                src:'.js',
+                //min:'.js'
+            }
+        }))
+        .pipe(gulp.dest('./dist/'))
+
+});
+
+
+gulp.task('default', ['build','libraries'], function () {
+  gulp.watch(['./src/**/**/*'], ['build']);
+});
+
---
+++ src/react-simplex.js
@@ -0,0 +1,109 @@
+import UUID from "uuid";
+
+function SimplexStorage(){
+    this.listeners = [];
+    this.Storage = {};
+}
+
+
+SimplexStorage.prototype.get = function( name ){
+    return this.Storage[ name ];
+}
+
+SimplexStorage.prototype.set = function( name, scope = [] ){
+
+    if ( !this.hasOwnProperty( name ) ){
+
+        Object.defineProperty( this, name, {
+            set: ( scope ) => {
+                this.Storage[ name ] = scope;
+                this.trigger( name, scope );
+            },
+            get: ( prop )  => {
+                return this.Storage[ arguments[0] ];
+            }
+        });
+    }
+
+    this.Storage[ name ] = scope;
+    this.trigger( name, scope )
+}
+
+SimplexStorage.prototype.onChange = function( name , callback ){
+    this.listeners.push( {name: name, callback: callback} );
+}
+
+SimplexStorage.prototype.trigger = function( name , scope ){
+    var result = null;
+
+    this.listeners.forEach(function(event) {
+        if ( event.name.indexOf( name ) == 0 ) {
+            if ( event.callback ){
+                result = event.callback.call(scope,  { [name]: scope} );
+            }
+        }
+    });
+    return result;
+}
+
+SimplexStorage.prototype.remove = function( name ){
+    for(var i in this.listeners){
+        if ( this.listeners[i].name == name ){
+            delete this.listeners[i];
+        }
+    }
+    this.listeners = this.listeners.filter(function(e){return e});
+}   
+
+window.Simplex = new SimplexStorage();
+
+
+
+
+
+
+
+
+window.SimplexConnect = function(Component, props = [] ) {
+    if ( !Array.isArray( props ) ){
+        console.error( 'SimplexConnect props must be an array' );
+        return;
+    }
+    const Key = UUID.v4();
+
+    const Connected = React.createClass({
+        getInitialState() {
+
+            var DefaultState = {};
+            props.forEach( ( prop )=>{
+                DefaultState[ prop ] = Simplex[ prop ];
+            });
+
+            return DefaultState;
+        },
+          
+        componentDidMount() {
+            props.forEach( ( prop )=>{
+                Simplex.onChange( prop + '.' + Key, ( scope )=>{
+                    this.setState( scope );
+                });
+            });
+        },
+
+        componentWillUnmount() {
+            props.forEach( ( prop )=>{
+                Simplex.remove( prop + '.' + Key )
+            });
+        },
+
+        render() {
+            return <Component {...this.props} {...this.state} />;
+        }
+    });
+    return Connected;
+};
+
+
+
+
+export default Simplex;
